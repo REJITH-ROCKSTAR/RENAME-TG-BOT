97dc9b7fcbefaca07a32066d3c8b6be1f6069262
℃linton Abraꫝam <70835540+Clinton-Abraham@users.noreply.github.com>
2020-11-02 16:36:48 +0530

    Add files via upload

---
+++ translation.py
@@ -0,0 +1,34 @@
+class Translation(object):
+    START_TEXT = """Please send me a telegram file and reply to an file with /rename Name.Extension\n
+    Do /help for more"""
+
+    RENAME_403_ERR = "<b>Rᴇɴᴀᴍᴇ Eʀʀᴏʀ Tʀʏ Aɢᴀɪɴ 🤯</b>"
+    BANNED_USER_TEXT = "<b>Yᴏᴜ Cᴀɴ'ᴛ Rᴜɴ Mᴇ Nᴏᴡ!</b>" 
+    ABS_TEXT = "Wʜᴀᴛ Aʀᴇ Yᴏᴜ Tʀʏɪɴɢ Tᴏ Dᴏ,Mᴀᴛᴇ?"
+    UPGRADE_TEXT = "<b>No need to upgrade</b>"
+    DOWNLOAD_START = "<b>Trying to download....</b>"
+    UPLOAD_START = "<b>Triying to upload....</b>"
+    RCHD_TG_API_LIMIT = "Downloaded in {} seconds.\nDetected File Size: {}\nSorry WTF Do You Think! I'll Upload It?"
+    NOT_AUTH_USER_TEXT = "CONTACT @Clinton_Abraham "
+    NOT_AUTH_USER_TEXT_FILE_SIZE = "IF You Got This Message You Should Contact @Clinton_Abraham"
+    SAVED_CUSTOM_THUMB_NAIL = "<b>Tʜᴜᴍʙɴᴀɪʟ Sᴀᴠᴇᴅ ✅</b>"
+    DEL_ETED_CUSTOM_THUMB_NAIL = "<b>Tʜᴜᴍʙɴᴀɪʟ Dᴇʟᴇᴛᴇᴅ ✅</b>"
+    FF_MPEG_DEL_ETED_CUSTOM_MEDIA = "Media cleared succesfully."
+    SAVED_RECVD_DOC_FILE = "<b>Tʀʏɪɴɢ Tᴏ Uᴘʟᴏᴀᴅ</b>"
+    REPLY_TO_DOC_FOR_RENAME_FILE = "<b>Pʟᴇᴀꜱᴇ Rᴇᴘʟʏ Tᴏ Aɴ Fɪʟᴇ Wɪᴛʜ /rename Fɪʟᴇ Nᴀᴍᴇ.Exᴛᴇɴꜱɪᴏɴ Tᴏ Rᴇɴᴀᴍᴇ ᴀ Fɪʟᴇ</b>"
+    REPLY_TO_DOC_FOR_C2V = "<b>Pʟᴇᴀꜱᴇ Rᴇᴘʟʏ Tᴏ Aɴ Fɪʟᴇ Wɪᴛʜ /c2v Tᴏ Cᴏɴᴠᴇʀᴛ Iᴛ Iɴᴛᴏ Sᴛʀᴇᴀᴍᴀʙʟᴇ Vɪᴅᴇᴏ Fɪʟᴇ</b>"
+    REPLY_TO_DOC_FOR_C2A = "<b> Please Reply To An File/video With /c2a To Convert It Into Audio File</b>"
+    CUSTOM_CAPTION_UL_FILE = " "
+    NO_CUSTOM_THUMB_NAIL_FOUND = "<b>Nᴏ Tʜᴜᴍʙɴᴀɪʟ Fᴏᴜɴᴅ 😟</b>"
+    USER_ADDED_TO_DB = "User <a href='tg://user?id={}'>{}</a> added to {} till {}."
+    IFLONG_FILE_NAME = """<b>Yᴏᴜ Gᴏᴛᴛᴀ Bᴇ Kɪᴅᴅɪɴɢ Mᴇ..Dᴇᴄʀᴇᴀꜱᴇ Tʜᴇ Nᴜᴍʙᴇʀ Oꜰ Lᴇᴛᴛᴇʀꜱ 😕</b>"""
+    AFTER_SUCCESSFUL_UPLOAD_MSG = "<b>Tʜᴀɴᴋ ᴜ Fᴏʀ Uꜱɪɴɢ Mᴇ</b>"
+    ABOUT_ME = "<b>Nothing</b>"
+
+
+    HELP_USER = """<b>Hᴏᴡ Tᴏ Uꜱᴇ Mᴇ</b>
+    
+<b> Sᴇɴᴅ ᴍᴇ ᴀɴʏ ꜰɪʟᴇ ꜰʀᴏᴍ ᴛᴇʟᴇɢʀᴀᴍ</b>\n
+<b> Sᴇɴᴅ ᴀ ᴘʜᴏᴛᴏ ᴛᴏ ᴍᴀᴋᴇ ɪᴛ ᴀꜱ ᴘᴇʀᴍᴀɴᴇɴᴛ ᴛʜᴜᴍʙɴᴀɪʟ</b>\n
+<b> Rᴇᴘʟʏ ᴛᴏ ᴛʜᴀᴛ ꜰɪʟᴇ ᴡɪᴛʜ /rename ɴᴇᴡ ꜰɪʟᴇ ɴᴀᴍᴇ.ᴇxᴛɴꜱɪᴏɴ</b>\n
+<b> I ᴄᴀɴ ᴀʟꜱᴏ ᴄᴏɴᴠᴇʀᴛ ꜰɪʟᴇꜱ ɪɴᴛᴏ ᴠɪᴅᴇᴏ ᴛʜʀᴏᴜɢʜ /c2v ᴄᴏᴍᴍᴀɴᴅ</b>"""
